2f56ec8e29c38d65bbf4e1061b939e837b6dbd98
Eugene Baskov <ebaskiv@gmail.com>
2016-12-28 11:58:33 +0200

    try sqlite

--- news/NewsDB.class.php
+++ news/NewsDB.class.php
@@ -1,1 +1,31 @@
 <?php
+require_once "INewsDB.class.php";
+class NewsDB implements INewsDB{
+	const DB_NAME = "news.db";
+	private $_db = null;
+
+	function __get($name) {
+		if($name == "db")
+			return $this->_db;
+		throw new Exception("Uknown property", 1);
+		
+	}
+	
+	function __construct() {
+		$this->_db = new SQLite3(self::DB_NAME);
+	}
+
+	function saveNews($title, $category, $description, $source) {
+
+	}
+	
+	public function getNews() {
+
+	}
+
+	public function deleteNews($id) {
+
+	}
+}
+
+$_db = new NewsDB();
